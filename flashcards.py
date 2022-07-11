56a94be02b251abca65f5f63cc5c06b72f621bd8
Nate Wood <99522059+natewoodprojects@users.noreply.github.com>
2022-07-11 11:42:03 -0500

    more

--- flashcards.py
+++ flashcards.py
@@ -1,21 +1,8 @@
-from datetime import datetime
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/") # @ denotes a 
 def welcome():
-    return "Welcome to my Flash Cards application!"
+    return render_template("welcome.html")
 
-
-@app.route("/date") # @ denotes a 
-def date():
-    return "This page was served at " + str(datetime.now())
-
-counter = 0
-
-@app.route("/count_views") # @ denotes a 
-def count_views():
-    global counter
-    counter += 1
-    return "This page was served " + str(counter) + " times"
