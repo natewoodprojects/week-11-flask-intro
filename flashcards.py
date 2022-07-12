f8dc2f338afabdd1eae98c6ceffffb012273cb89
Nate Wood <99522059+natewoodprojects@users.noreply.github.com>
2022-07-12 09:14:38 -0500

    more

--- flashcards.py
+++ flashcards.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, abort, jsonify
-from model import db
+from flask import (Flask, render_template, abort, jsonify, request, redirect, url_for)
+
+from model import db, save_db
+
 app = Flask(__name__)
 
 @app.route("/") # @ denotes a 
@@ -16,6 +18,30 @@ def card_view(index):
         return render_template('card.html', card=card, index=index, max_index=len(db)-1)
     except IndexError:
         abort(404)
+
+@app.route('/add_card', methods=['GET','POST'])
+def add_card():
+    if request.method == "POST":
+        card = {'question': request.form['question'],
+                'answer': request.form['answer']}
+        db.append(card)
+        save_db()
+        return redirect(url_for('card_view', index=len(db)-1))
+    else:   
+        return render_template('add_card.html')
+
+@app.route('/remove_card/<int:index>', methods=['GET','POST'])
+def remove_card(index):
+    try:
+        if request.method == "POST":
+            del db[index]
+            save_db()
+            return redirect(url_for('welcome'))
+        else:
+            return render_template('remove_card.html', card=db[index])
+    except IndexError:
+        abort(404)
+
 
 @app.route("/api/card/")
 def api_card_list():
